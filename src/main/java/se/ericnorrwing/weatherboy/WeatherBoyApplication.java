2155bdedde9dac00ae64c402469db4b0e43bff70
EricNorrwing <eric.norrwing@gmail.com>
2024-11-10 16:52:33 +0100

    Mapped some Json names from the API

--- src/main/java/se/ericnorrwing/weatherboy/WeatherBoyApplication.java
+++ src/main/java/se/ericnorrwing/weatherboy/WeatherBoyApplication.java
@@ -3,6 +3,7 @@ package se.ericnorrwing.weatherboy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//WOULDN'T YOU LIKE TO KNOW
 @SpringBootApplication
 public class WeatherBoyApplication {
 
