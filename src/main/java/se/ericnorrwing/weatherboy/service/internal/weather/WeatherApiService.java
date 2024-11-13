b0844c8cdb1aed464cfe74876e487bafd279ff4b
EricNorrwing <Eric.Norrwing@gmail.com>
2024-11-13 09:20:42 +0100

    Added some boilerplate service, naming to be changed

---
+++ src/main/java/se/ericnorrwing/weatherboy/service/internal/weather/WeatherApiService.java
@@ -0,0 +1,4 @@
+package se.ericnorrwing.weatherboy.service.internal.weather;
+
+public interface WeatherApiService {
+}
---
+++ src/main/java/se/ericnorrwing/weatherboy/service/internal/weather/WeatherApiServiceCore.java
@@ -0,0 +1,8 @@
+package se.ericnorrwing.weatherboy.service.internal.weather;
+
+import org.springframework.stereotype.Service;
+//TODO RENAME?
+
+@Service
+public class WeatherApiServiceCore implements WeatherApiService{
+}
